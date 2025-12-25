50829ab277de48187378bf9371af8e0690725f37
Sebas16608 <asebasrr444@gmail.com>
2025-12-25 17:32:04 -0600

    Creando los metodos get del controlador de mensaje

---
+++ src/controllers/mensaje-controller.js
@@ -0,0 +1,22 @@
+const Mensaje = require("../models/mensaje");
+
+const getAllMessages = async (req, res) => {
+    try{
+        const msg = await Mensaje.findAll();
+        res.json(msg);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: "Internal Server Error"});
+    };
+};
+
+const getMessageById = async (req, res) => {
+    try {
+        const msg = await Mensaje.findByPk(req.params.id)
+        res.json(msg);
+    } catch (err) {
+        console.log(err);
+        res.status(404).json({ error: "Not Found"});
+    };
+};
+
