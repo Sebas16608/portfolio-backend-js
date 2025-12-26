d72c082e3689111f6008f15d45d535015c4d444c
Sebas16608 <asebasrr444@gmail.com>
2025-12-25 19:12:28 -0600

    agregando el codigo http para decir que fue creado

--- src/controllers/mensaje-controller.js
+++ src/controllers/mensaje-controller.js
@@ -16,7 +16,7 @@ const getMessageById = async (req, res) => {
         const msg = await Mensaje.findByPk(req.params.id)
         res.json(msg);
         if (!msg) return res.status(404).json({error: "No se encontro el mensaje"});
-        
+
     } catch (err) {
         console.log(err);
         res.status(404).json({ error: "Not Found"});
@@ -27,7 +27,7 @@ const postMessage = async (req, res) => {
     try {
         const { name, email, message } = req.body;
         const msg = await Mensaje.create( {name, email, message });
-        res.json(msg);
+        res.status(201).json(msg);
     } catch (err) {
         console.log(err);
         res.status(400).json({ error: "Bad Request"});
